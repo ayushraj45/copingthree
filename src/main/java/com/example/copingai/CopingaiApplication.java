70b8e9a296b3a05aae816c1301c1fec01fa128b3
Ayush <pandeyayushraj4@gmail.com>
2024-03-26 16:53:03 +0000

    inital Commit

---
+++ src/main/java/com/example/copingai/CopingaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.copingai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopingaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopingaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/copingai/CopingaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copingai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopingaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
